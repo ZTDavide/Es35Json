513bf4e6b8a83cfb47337373f53b7b9ec757afa5
jakubbocian <jakub.bocian@mpolo.it>
2023-02-07 10:20:35 +0100

    Creazione file json con gare

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
+import com.google.gson.Gson;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Gson gson = new Gson();
     }
 }
