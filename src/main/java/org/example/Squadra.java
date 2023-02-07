c618959adf68f27bef4cb765eef758dec5e81452
ZTDavide <dadocarto503@gmail.com>
2023-02-07 11:43:23 +0100

    Somma dei punti

---
+++ src/main/java/org/example/Squadra.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Squadra {
+
+    String nome;
+    int punteggio;
+    int tempo;
+
+    public Squadra(String nome, int punteggio, int tempo){
+
+        this.nome = nome;
+        this.tempo = tempo;
+        this.punteggio = punteggio;
+
+    }
+
+}
