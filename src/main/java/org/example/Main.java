b9951d2d8990c18d667151368891e81a5aa5a380
jakubbocian <jakub.bocian@mpolo.it>
2023-02-07 10:25:44 +0100

    Lettura dal file json in formato String

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,17 @@ package org.example;
 
 import com.google.gson.Gson;
 
+import java.io.IOException;
+import java.nio.file.*;
+
 public class Main {
-    public static void main(String[] args) {
+    private static String readFromFile(String path) throws IOException {
+        byte[] content = Files.readAllBytes(Paths.get(path));
+        return new String(content);
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(readFromFile("src/main/resources/gare.json"));
         Gson gson = new Gson();
     }
 }
