93443b3a037e67fd84e1c52c8a34a89b33d1a68d
ZTDavide <dadocarto503@gmail.com>
2023-02-07 10:27:28 +0100

    Classe gara

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
     }
 }
